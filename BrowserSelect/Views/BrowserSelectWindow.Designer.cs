e66beaad2ff54c714a8b8c1383b8f8f23253a239
Great Dead Cthulhu <dragnilar@gmail.com>
2019-03-03 21:33:41 -0500

    Changed help button to SVG

--- BrowserSelect/Views/BrowserSelectWindow.Designer.cs
+++ BrowserSelect/Views/BrowserSelectWindow.Designer.cs
@@ -24,26 +24,28 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BrowserSelectWindow));
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.btn_help = new System.Windows.Forms.Button();
+            this.btn_help = new DevExpress.XtraEditors.SimpleButton();
             this.SuspendLayout();
             // 
             // btn_help
             // 
             this.btn_help.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_help.BackgroundImage = global::BrowserSelect.Properties.Resources.Button_help_icon;
-            this.btn_help.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_help.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_help.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
             this.btn_help.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_help.FlatAppearance.BorderSize = 0;
-            this.btn_help.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_help.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleCenter;
+            this.btn_help.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_help.ImageOptions.SvgImage")));
+            this.btn_help.ImageOptions.SvgImageSize = new System.Drawing.Size(30, 30);
             this.btn_help.Location = new System.Drawing.Point(97, 155);
             this.btn_help.Name = "btn_help";
             this.btn_help.Size = new System.Drawing.Size(25, 25);
             this.btn_help.TabIndex = 0;
-            this.btn_help.UseVisualStyleBackColor = true;
             this.btn_help.Click += new System.EventHandler(this.btn_help_Click);
             // 
-            // Form1
+            // BrowserSelectWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
@@ -62,7 +64,7 @@
 
         #endregion
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button btn_help;
+        private DevExpress.XtraEditors.SimpleButton btn_help;
     }
 }
 
